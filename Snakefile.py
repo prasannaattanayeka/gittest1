c746ef124aa79a8e30c346d440120526b9fa6580
prasanna attanayaka <prasanna attanayeka>
2022-08-12 12:44:48 +0530

    Snakefile.py

--- Snakefile.py
+++ Snakefile.py
@@ -1,5 +1,7 @@
 print("This line will be printed..............................")
 #ADDED NEW LINE V.1
 print("This line will be printed..............................")
+#ADDED NEW LINE V.2
+print("This line will be printed V.2..............................")
 
 
